0b32ae352b09cfbb69e56ed717f57c7b805a0464
Avtar Prajapati <prajaptiavtar@gmail.com>
2020-05-23 11:07:19 +0530

    post like issue undefine

--- src/components/Posts/PostList.js
+++ src/components/Posts/PostList.js
@@ -111,7 +111,7 @@ class PostList extends Component {
                     color: `${this.likeColor(post.like)}`
                   }}
                 ></i>
-                {console.log(post.like)} Likes
+                {console.log(`post ${post.id} ${post.like}`)} Likes
               </span>
             </div>
           </div>
